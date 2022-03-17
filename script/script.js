a140a002e75c9f6450ac64bf07b6d0c81314413a
Taufik Nurrahman <official.taufik980@gmail.com>
2022-03-17 10:07:30 +0700

    updated

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 // slider
 const nextIcon = '<img src="images/next.png">';
+const backIcon = '<img src="images/back.png">';
 
 $(".owl-carousel").owlCarousel({
-  loop: true,
+  loop: false,
   margin: 10,
   nav: true,
   dots: false,
-  navText: [false, nextIcon],
+  navText: [backIcon, nextIcon],
   responsive: {
     0: {
       items: 1,
